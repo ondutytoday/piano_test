1630316602613f165f5dcf576d5e6fbf8c2bc08e
Elena Vasilyeva <elena.vasilyeva@magenta-technology.com>
2022-01-22 16:53:20 +0400

    Wake up, Neo... The Matrix has yoy... Follow the white rabbit.

---
+++ src/test/java/org/test/piano/service/impl/FileReadingServiceImplTest.java
@@ -0,0 +1,9 @@
+package org.test.piano.service.impl;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FileReadingServiceImplTest {
+
+}
